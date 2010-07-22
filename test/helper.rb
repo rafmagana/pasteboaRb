5ade0bd213c17647578bb3d8c937bfb8e279fcb3
Rafael Magana <raf.magana@gmail.com>
2010-07-21 22:42:39 -0500

    tests

--- test/helper.rb
+++ test/helper.rb
@@ -7,4 +7,16 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'pasteboaRb'
 
 class Test::Unit::TestCase
+  def self.supported? platform
+    supported = ''
+    PasteboaRb::Platform::Supported.constants.each_with_index do |k, i|
+      if platform == PasteboaRb::Platform::Supported::class_eval(k)
+        supported = true
+        break
+      else
+        supported = false
+      end
+    end
+    supported
+  end
 end
--- test/test_pasteboaRb.rb
+++ test/test_pasteboaRb.rb
@@ -1,7 +1,67 @@
 require 'helper'
 
 class TestPasteboarb < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  
+  context "Platform.OS" do
+    should "be defined and return a symbol" do
+      assert_nothing_thrown { PasteboaRb::Platform.OS }
+      assert_instance_of Symbol, PasteboaRb::Platform.OS
+    end
+  end
+  
+  context "Object" do
+    should "have a to_pboard method" do
+      assert_respond_to "test", :to_pboard
+    end
+    
+    should "have to_pb method" do
+      assert_respond_to "test", :to_pb
+    end
+  end
+  
+  context "PasteboaRb::TO_PBOARD and PasteboaRb::PASTE" do
+    should "exist" do
+      assert_nothing_thrown { PasteboaRb::TO_PBOARD }
+      assert_nothing_thrown { PasteboaRb::PASTE }
+    end
+    
+    should "return a proc" do
+      assert_instance_of Proc, PasteboaRb::TO_PBOARD
+      assert_instance_of Proc, PasteboaRb::PASTE
+    end
+  end
+  
+  if supported? PasteboaRb::Platform.OS
+    context "Specifying the pasteboard" do
+      should "set the right pasteboard" do
+        text = "testing"
+      
+        text.to_pboard :general
+        assert_equal text, paste(:general)
+        "".to_pboard :general
+      
+        text.to_pboard :find
+        assert_equal text, paste(:find)
+        "".to_pboard :find
+      
+        text.to_pboard :ruler
+        assert_equal text, paste(:ruler)
+        "".to_pboard :ruler
+      
+        text.to_pboard :font
+        assert_equal text, paste(:font)
+        "".to_pboard :font
+      
+      end
+    end
+  else
+    context "Unsupported platform" do
+      should "return false when calling TO_PBOARD" do
+        assert_equal false, PasteboaRb::TO_PBOARD.call('', '')
+      end
+      should "return a warning when calling PASTE" do
+        assert_equal "Platform not supported, only Mac OS X for the moment", PasteboaRb::PASTE.call('')
+      end
+    end
   end
 end
